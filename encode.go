eef6d5c0569f41113eb9ebd36402eae7829b5fab
Irkham Widhi Saputro <fishblacklance@gmail.com>
2020-03-19 22:59:54 +0700

    Add reconnect flags at ffmpeg command

--- encode.go
+++ encode.go
@@ -198,6 +198,10 @@ func (e *EncodeSession) run() {
 
 	// Launch ffmpeg with a variety of different fruits and goodies mixed togheter
 	args := []string{
+		"-reconnect", "1",
+		"-reconnect_at_eof", "1",
+		"-reconnect_streamed", "1",
+		"-reconnect_delay_max", "4294",
 		"-stats",
 		"-i", inFile,
 		"-map", "0:a",
